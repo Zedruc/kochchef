9825a528c9654ef75921a46a9702cebaadfde459
Zedruc <69540929+Zedruc@users.noreply.github.com>
2025-10-14 19:10:32 +0200

    English is better

--- design/database.sql
+++ design/database.sql
@@ -1,75 +1,62 @@
 CREATE DATABASE IF NOT EXISTS `kochchef`;
-
 USE `kochchef`;
 
-CREATE TABLE IF NOT EXISTS `benutzer` (
-  `benutzer_id` int PRIMARY KEY AUTO_INCREMENT,
-  `vorname` varchar(128),
-  `nachname` varchar(128),
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int PRIMARY KEY AUTO_INCREMENT,
+  `firstname` varchar(128),
+  `surname` varchar(128),
   `username` varchar(128) UNIQUE,
   `email` varchar(128) UNIQUE,
-  `passwort` char(60) COMMENT '60 byte bcrypt hash'
+  `password` char(60) COMMENT '60 byte bcrypt hash'
 );
 
-CREATE TABLE IF NOT EXISTS `bewertung` (
-  `benutzer_id` int NOT NULL,
-  `inhalt` varchar(1024),
-  `sterne` int,
-  `rezept_id` int NOT NULL,
-  PRIMARY KEY (`benutzer_id`, `rezept_id`)
-  CHECK (`bewertung` >= 0 AND `bewertung` <= 5)
-);
-
-CREATE TABLE IF NOT EXISTS `gericht` (
-  `gericht_id` int PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `meal` (
+  `meal_id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(128) UNIQUE NOT NULL,
-  `beschreibung` varchar(512)
+  `description` varchar(512)
 );
 
-CREATE TABLE IF NOT EXISTS `gericht_alias` (
+CREATE TABLE IF NOT EXISTS `meal_alias` (
   `name` varchar(128) NOT NULL,
-  `gericht_id` int NOT NULL,
-  PRIMARY KEY (`gericht_id`, `name`)
+  `meal_id` int NOT NULL,
+  PRIMARY KEY (`meal_id`, `name`),
+  CONSTRAINT fk_alias_meal FOREIGN KEY (`meal_id`) REFERENCES meal(`meal_id`)
 );
 
-CREATE TABLE IF NOT EXISTS `rezept` (
-  `rezept_id` int PRIMARY KEY AUTO_INCREMENT,
-  `gericht_id` int NOT NULL,
-  `autor_id` int NOT NULL,
+CREATE TABLE IF NOT EXISTS `recipe` (
+  `recipe_id` int PRIMARY KEY AUTO_INCREMENT,
+  `meal_id` int NOT NULL,
+  `author_id` int NOT NULL,
   `name` varchar(128) NOT NULL,
-  `anweisung` text NOT NULL COMMENT 'Vllt. Markdown?'
+  `instructions` text NOT NULL COMMENT 'Vllt. Markdown?',
+  UNIQUE KEY `recipe_meal_name` (`meal_id`, `name`),
+  CONSTRAINT fk_recipe_meal FOREIGN KEY (`meal_id`) REFERENCES meal(`meal_id`),
+  CONSTRAINT fk_recipe_author FOREIGN KEY (`author_id`) REFERENCES user(`user_id`)
 );
 
-CREATE TABLE IF NOT EXISTS `zutat` (
-  `zutat_id` int PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `ingredient` (
+  `ingredient_id` int PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(64),
-  `preis` decimal(6,2)
+  `price` decimal(6,2)
 );
 
-CREATE TABLE IF NOT EXISTS `benoetigt` (
-  `rezept_id` int NOT NULL,
-  `zutat_id` int NOT NULL,
-  `menge` decimal(6,2),
-  `einheit` varchar(16),
-  PRIMARY KEY (`rezept_id`, `zutat_id`)
+CREATE TABLE IF NOT EXISTS `needed` (
+  `recipe_id` int NOT NULL,
+  `ingredient_id` int NOT NULL,
+  `amount` decimal(6,2),
+  `unit` varchar(16),
+  PRIMARY KEY (`recipe_id`, `ingredient_id`),
+  CONSTRAINT fk_needed_recipe FOREIGN KEY (`recipe_id`) REFERENCES recipe(`recipe_id`),
+  CONSTRAINT fk_needed_ingredient FOREIGN KEY (`ingredient_id`) REFERENCES ingredient(`ingredient_id`)
 );
 
-CREATE UNIQUE INDEX `rezept_index_0` ON `rezept` (`gericht_id`, `name`);
-
-CREATE INDEX `rezept_index_1` ON `rezept` (`rezept_id`);
-
-ALTER TABLE `bewertung` COMMENT = 'CHECK (sterne BETWEEN 0 AND 5)';
-
-ALTER TABLE `benutzer` ADD FOREIGN KEY (`benutzer_id`) REFERENCES `bewertung` (`benutzer_id`);
-
-ALTER TABLE `rezept` ADD FOREIGN KEY (`rezept_id`) REFERENCES `bewertung` (`rezept_id`);
-
-ALTER TABLE `gericht` ADD FOREIGN KEY (`gericht_id`) REFERENCES `gericht_alias` (`gericht_id`);
-
-ALTER TABLE `gericht` ADD FOREIGN KEY (`gericht_id`) REFERENCES `rezept` (`gericht_id`);
-
-ALTER TABLE `benutzer` ADD FOREIGN KEY (`benutzer_id`) REFERENCES `rezept` (`autor_id`);
-
-ALTER TABLE `rezept` ADD FOREIGN KEY (`rezept_id`) REFERENCES `benoetigt` (`rezept_id`);
-
-ALTER TABLE `zutat` ADD FOREIGN KEY (`zutat_id`) REFERENCES `benoetigt` (`zutat_id`);
+CREATE TABLE IF NOT EXISTS `rating` (
+  `user_id` int NOT NULL,
+  `recipe_id` int NOT NULL,
+  `content` varchar(1024),
+  `stars` int,
+  PRIMARY KEY (`user_id`, `recipe_id`),
+  CONSTRAINT fk_rating_user FOREIGN KEY (`user_id`) REFERENCES user(`user_id`),
+  CONSTRAINT fk_rating_recipe FOREIGN KEY (`recipe_id`) REFERENCES recipe(`recipe_id`),
+  CHECK (`stars` BETWEEN 0 AND 5)
+);
